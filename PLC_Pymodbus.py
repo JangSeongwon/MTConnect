dd7d08f5a44db0502e71015cb0fb1724054f7534
Jang <87692888+JangSeongwon@users.noreply.github.com>
2025-02-27 19:09:41 +0900

    Update PLC_Pymodbus.py

--- PLC_Pymodbus.py
+++ PLC_Pymodbus.py
@@ -1,34 +1,32 @@
 # Python 3.11.9 버전 작성
-# PLC PC 기본 통신 코드
+# PLC PC 기본 통신 코드 
 import logging
-from pymodbus.client.sync import ModbusSerialClient
-logging.basicConfig(level=logging.DEBUG)
+from pymodbus.client import ModbusSerialClient
+# logging.basicConfig(level=logging.DEBUG)
 
 # Modbus RTU 설정
 client = ModbusSerialClient(
-    method='rtu',
     port='COM5',  # Windows: 'COM5', Linux: '/dev/ttyUSB0'
-    baudrate=38400,
+    baudrate=19200,
     parity='N',
     stopbits=1,
     bytesize=8,
-    timeout=5,
-    strict=False
+    timeout= 1
 )
 
 client.connect()
-print("Modbus connected")
-
-# result = client.read_coils(1)
-# print(result)
+# result1 = client.read_coils(1)
+# print(result1)
 
 if client.connect():
-    write_result = client.write_register(address=1, value=33, slave=1)
-    print('Write', write_result)
+    print("Modbus connected")
+    # write_result = client.write_register(address=105, value=33, slave=1)
+    # print('Write', write_result)
 
+    print('Receiving Start')
     # PLC에서 Holding Register 0번 주소 (D100) 읽기
-    # count=1 은 16비트로 1개 WORD
-    result = client.read_holding_registers(address=1000, count=1, slave=1)
+    # count=1 은 16bit로 1개 WORD
+    result = client.read_holding_registers( address=100, count=1, slave=1)
     print('Read', result)
 
     if result.isError():
@@ -39,3 +37,4 @@ if client.connect():
     client.close()
 else:
     print("Modbus 연결 실패!")
+
